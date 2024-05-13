c253c6d927446761e4cac26dfad10a0d7e95a444
EC2 Default User <ec2-user@ip-172-31-12-159.ec2.internal>
2024-05-13 16:55:18 +0000

    app created, EditTask componente pending

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { AddTaskComponent } from './add-task/add-task.component';
+import { LocalStorageDataComponent } from './local-storage-data/local-storage-data.component';
+import { DeleteItemComponent } from './delete-item/delete-item.component';
+const routes: Routes = [{
+    path:'', component:AddTaskComponent,pathMatch:'full'
+  },
+  
+  {
+    path:'my-tasks', component: LocalStorageDataComponent
+  },
+  {
+    path:'**', component:AddTaskComponent,pathMatch:'full'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { LocalStorageDataComponent } from './local-storage-data/local-storage-data.component';
+import { DeleteItemComponent } from './delete-item/delete-item.component';
+import { EditTaskComponent } from './edit-task/edit-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTaskComponent,
+    LocalStorageDataComponent,
+    DeleteItemComponent,
+    EditTaskComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
